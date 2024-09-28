737d8dc2e1c9cd1c1a3243553dabdcd90ac39989
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-09-28 15:13:32 -0400

    Developing the code...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use crossterm::event::read;
+use crossterm::event::{read, Event, KeyCode, KeyEventKind};
+use std::fs::read_to_string;
 
 fn main() {
+    let file_content = read_to_string("typing.txt").unwrap();
+    let mut user_input = String::new();
+
     loop {
-        println!("{:?}", read().unwrap());
+        println!("{file_content}");
+        println!("{user_input}_");
+
+        if let Event::Key(key_event) = read().unwrap() {
+            if key_event.kind == KeyEventKind::Press {
+                match key_event.code {
+                    KeyCode::Backspace => {
+                        user_input.pop();
+                    }
+                    KeyCode::Esc => break,
+                    KeyCode::Char(c) => {
+                        user_input.push(c);
+                    }
+                    _ => {}
+                }
+            }
+        }
     }
 }
